8ded76828f1be807e26a0a63d63afb817e490c24
Greg Kazmierczak <kgrzesiu@gmail.com>
2020-05-11 20:25:38 +0200

    Async action with state

--- src/store/todos/asyncActions.ts
+++ src/store/todos/asyncActions.ts
@@ -3,9 +3,14 @@ import { ThunkAction } from "redux-thunk";
 import { RootState } from "..";
 import { IAddTodoAction } from "./types"
 
-export function asyncAddTodo(text: string): ThunkAction<void, RootState, null, IAddTodoAction> {
-    return function (dispatch:any) {
+export function asyncAddTodo(
+    text: string
+): ThunkAction<void, RootState, null, IAddTodoAction> {
+    return (dispatch: any, getState: () => RootState) => {
         setTimeout(() => {
+            //get the state in the async action
+            console.log(getState())
+            //dispatch the proper action
             dispatch(addTodo(text))
         }, 2000);
     }
