b737308859bf23018fe5fa0a90b2f526840eba7f
Greg Kazmierczak <kgrzesiu@gmail.com>
2020-05-11 23:16:16 +0200

    Basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 import './css/App.css';
 
@@ -16,15 +16,19 @@ class App extends React.Component {
           <header className="App-header">
             <nav className="Navigation">
               <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/todos">Todos</Link></li>
-                <li><Link to="/stats">Stats</Link></li>
+                <li><NavLink to="/" exact activeClassName="customActive">Home</NavLink></li>
+                <li><NavLink to="/todos">Todos</NavLink></li>
+                <li><NavLink to={{
+                                pathname: '/stats',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>Stats</NavLink></li>
               </ul>
             </nav>
             <Route path="/" exact render={()=> <h4>Home page of todos</h4>}/>
 
-            <Route path="/todos" component={AddTodo}/>
-            <Route path="/todos" component={TodoList}/>
+            <Route path="/(todos|todos/:id)" component={AddTodo}/>
+            <Route path="/(todos|todos/:id)" component={TodoList}/>
             <Route path="/todos" component={Footer}/>
 
             <Route path="/stats" exact component={Stats}/>
