9d79524594cfd75cef6b431c264ea57519ca3b7c
Greg Kazmierczak <kgrzesiu@gmail.com>
2020-05-11 19:16:02 +0200

    Adding middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from "./store";
 import * as serviceWorker from './serviceWorker';
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { rootReducer } from './store';
+import './index.css';
 
-const store = createStore(rootReducer);
+const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,25 @@
-import { combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import todosReducer from './todos/reducers'
 import visibilityFilterReducer from './visibilityFilters/reducers'
 
-export const rootReducer = combineReducers({
+const rootReducer = combineReducers({
   todos:todosReducer,
   visibilityFilter: visibilityFilterReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
+
+export default function configureStore() {
+  const middlewares = [thunkMiddleware];
+  const middleWareEnhancer = applyMiddleware(...middlewares);
+
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(middleWareEnhancer)  
+  );
+  return store;
+}
