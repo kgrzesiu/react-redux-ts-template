4e67bb29f055bbe02b6c1f615d25dafdc006344b
Greg Kazmierczak <kgrzesiu@gmail.com>
2020-05-11 17:59:33 +0200

    Workload

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { connect, ConnectedProps } from 'react-redux'
 import Link from './Link'
 import { VisibilityFilters } from '../../store/visibilityFilters/types'
+import { RootState } from '../../store'
 
 enum WORK_LOAD {
     doable = "doable",
@@ -12,8 +14,18 @@ interface IState {
     workloadState: WORK_LOAD;
 }
 
-class Footer extends Component<any, IState> {
+class Footer extends Component<ConnectedProps<typeof reduxConnector>, IState> {
     readonly state = { workloadState: WORK_LOAD.doable}
+
+    getWorkLoad(count:number):WORK_LOAD {
+        if (count < 3){
+            return WORK_LOAD.doable
+        } else if (count < 5){
+            return WORK_LOAD.hard
+        } else {
+            return WORK_LOAD.insance
+        }
+    }
 
     render() {
         return (
@@ -22,10 +34,16 @@ class Footer extends Component<any, IState> {
                 <Link filter={VisibilityFilters.SHOW_ALL}>All</Link>
                 <Link filter={VisibilityFilters.SHOW_ACTIVE}>Active</Link>
                 <Link filter={VisibilityFilters.SHOW_COMPLETED}>Completed</Link>
-                <div>State of todos: {this.state.workloadState}</div>
+                <div>Workload of todos: {this.getWorkLoad(this.props.todoCount)}</div>
             </div>
         )
     }
 }
 
-export default Footer
+const mapStateToProps = (state: RootState) => ({
+    todoCount: state.todos.length
+});
+
+const reduxConnector = connect(mapStateToProps)
+
+export default reduxConnector(Footer)
